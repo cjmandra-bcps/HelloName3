0c1f8412799608b52d7d1254ee6cedb7db92d781
Mandra <cjmandra@bcps.k12.md.us>
2025-11-10 12:23:23 -0500

    This is my commit for Monday Nov 10

---
+++ app/src/main/java/com/mervo/helloname3/MainActivity.java
@@ -0,0 +1,38 @@
+package com.mervo.helloname3;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    TextView myTextView;
+    EditText myEditText;
+    Button submitButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myTextView = (TextView) findViewById(R.id.greeting);
+        myEditText = (EditText) findViewById(R.id.typeName);
+        submitButton = (Button) findViewById(R.id.submitButton);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = myEditText.getText().toString();
+                myTextView.setText("Hello " + name + "!");
+            }
+
+        });
+    }
+}
